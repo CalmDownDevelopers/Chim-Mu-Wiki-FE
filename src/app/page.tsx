6b3500b64cb0f9c11867752b61ac7dce0077b3fd
root-zero-o <rootzero17@gmail.com>
2023-03-02 16:53:57 +0900

    ✨ feat : add Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-100">
+      <div>1</div>
+    </div>
+  );
 };
 
 export default Page;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import Logo from "../../assets/icons/logo.svg";
+import User from "../../assets/icons/user.svg";
+
 export const Header = () => {
-  return <div className="w-[100vw] h-4 bg-main">1</div>;
+  return (
+    <div className="w-[100vw] h-4 bg-main flex items-center justify-between">
+      <button type="button" className="p-1">
+        <Image src={Logo} alt="logo" width={100} />
+      </button>
+      <div className="flex gap-1 items-center ">
+        <input type="text" className="w-20 h-3 border-[1px] pl-1 " />
+        <button type="button" className="p-1">
+          <Image src={User} alt="user" width={40} />
+        </button>
+      </div>
+    </div>
+  );
 };
